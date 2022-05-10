2bd571d1bbb46db583dd5ba11dc891fa48bf1e21
nana-47 <nana.oikawa@rakus-partners.co.jp>
2022-05-10 13:42:39 +0900

    first commit

---
+++ src/test/java/com/example/GitSampleOikawaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSampleOikawaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
